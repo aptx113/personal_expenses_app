6e0559d3124a77906cd2e8c6b878d7574e0a3246
George <george.hs@hsress.com>
2022-03-28 17:32:45 +0800

    Add FAB

--- lib/main.dart
+++ lib/main.dart
@@ -21,22 +21,30 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('Flutter App')),
-        body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                width: double.infinity,
-                child: Card(
-                  color: Colors.blue,
-                  child: Text('Chart'),
-                  elevation: 5,
-                ),
+      appBar: AppBar(title: Text('Flutter App'), actions: <Widget>[
+        IconButton(onPressed: () {}, icon: Icon(Icons.add))
+      ]),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('Chart'),
+                elevation: 5,
               ),
-              const UserTransactions()
-            ],
-          ),
-        ));
+            ),
+            const UserTransactions()
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+    );
   }
 }
