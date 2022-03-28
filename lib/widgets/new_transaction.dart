850e3929790eb27d18a2bd05e58bc2d3ac815d4a
George <george.hs@hsress.com>
2022-03-28 17:24:35 +0800

    Modify input experience

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -9,6 +9,17 @@ class NewTransaction extends StatelessWidget {
   final Function onSumit;
   final titleController = TextEditingController();
 
+  void sumitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+
+    onSumit(enteredTitle, enteredAmount);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -21,14 +32,16 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: const InputDecoration(labelText: 'Title'),
               controller: titleController,
+              keyboardType: TextInputType.text,
             ),
             TextField(
               decoration: const InputDecoration(labelText: 'Amount'),
               controller: amountController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => sumitData(),
             ),
             TextButton(
-              onPressed: (() => onSumit(
-                  titleController.text, double.parse(amountController.text))),
+              onPressed: sumitData,
               child: const Text('Add Transaction'),
               style: TextButton.styleFrom(primary: Colors.purple),
             )
